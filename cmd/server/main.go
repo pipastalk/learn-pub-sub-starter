5d11ccab42b7dcb3847ec75860b7323d1c398820
Pipastalk <pipalabs@luckyfixes.co.uk>
2026-06-10 20:52:50 +0100

    Added rabbitmq_stomp, updated main to start a amqp connection to rabbit image

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/signal"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril server...")
+	var connectionStr = "amqp://guest:guest@localhost:5672/"
+	connection, err := amqp.Dial(connectionStr)
+	if err != nil {
+		fmt.Printf("Failed to connect to RabbitMQ: %s\n", err)
+		os.Exit(1)
+	}
+	defer connection.Close()
+	fmt.Println("Connected to RabbitMQ successfully!")
+	// wait for ctrl+c
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
+	fmt.Println("Server has been terminated")
+
 }
