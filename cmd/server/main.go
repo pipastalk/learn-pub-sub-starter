c3ace97ea8cec25cce9cf1db8c938faa3bae4517
Pipastalk <pipalabs@luckyfixes.co.uk>
2026-06-10 21:39:00 +0100

    updated go.mod to point to fork, created a publishJson function and pubsub package

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"os/signal"
 
+	"github.com/pipastalk/learn-pub-sub-starter/internal/pubsub"
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
@@ -18,6 +19,18 @@ func main() {
 	}
 	defer connection.Close()
 	fmt.Println("Connected to RabbitMQ successfully!")
+	ch, err := connection.Channel()
+	if err != nil {
+		fmt.Printf("Failed to create channel: %s\n", err)
+		os.Exit(1)
+	}
+	defer ch.Close()
+	err = pubsub.PublishJSON(ch, ExchangePerilDirect, PauseKey, PlayingState{IsPaused: true})
+	if err != nil {
+		fmt.Printf("Failed to publish JSON: %s\n", err)
+		os.Exit(1)
+	}
+
 	// wait for ctrl+c
 	signalChan := make(chan os.Signal, 1)
 	signal.Notify(signalChan, os.Interrupt)
---
+++ internal/pubsub/publishjson.go
@@ -0,0 +1,29 @@
+package pubsub
+
+import (
+	"context"
+	"encoding/json"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func PublishJSON[T any](ch *amqp.Channel, exchange, key string, val T) error {
+	valBytes, err := json.Marshal(val)
+	if err != nil {
+		return err
+	}
+	err = ch.PublishWithContext(
+		context.Background(),
+		exchange,
+		key,
+		false,
+		false,
+		amqp.Publishing{
+			ContentType: "application/json",
+			Body:        valBytes,
+		},
+	)
+	//Should I do error check here and return nil?
+	return err
+
+}
